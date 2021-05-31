d45886133c89d72dbe117e18d224099c289267dd
Masahiro Masuda <masahi129@gmail.com>
2021-06-01 07:45:19 +0900

    add rewriter but got an error

--- ssd_mobilenet/rewrite_combined_nms.py
+++ ssd_mobilenet/rewrite_combined_nms.py
@@ -1,14 +1,28 @@
-from tvm import relay
-from tvm.relay import op
+from tvm.relay import op as _op
+from tvm.relay import expr as _expr
 from tvm.relay.dataflow_pattern import *
 
 
-def combined_nms_pattern(boxes, scores, max_boxes_per_class, iou_threshold, score_threshold, raw_boxes, raw_scores):
-    nms_out = is_op("vision.all_class_non_max_suppression")(boxes, scores, max_boxes_per_class, iou_threshold, score_threshold)
+def combined_nms_pattern(
+    boxes,
+    scores,
+    max_boxes_per_class,
+    iou_threshold,
+    score_threshold,
+    raw_boxes,
+    raw_scores,
+):
+    nms_out = is_op("vision.all_class_non_max_suppression")(
+        boxes, scores, max_boxes_per_class, iou_threshold, score_threshold
+    )
     selected_indices = is_tuple_get_item(nms_out, 0)
     num_detections = is_tuple_get_item(nms_out, 1)
-    v478 = is_op("dyn.strided_slice")(selected_indices, wildcard(), num_detections, wildcard())
-    v479 = is_op("dyn.strided_slice")(selected_indices, wildcard(), num_detections, wildcard())
+    v478 = is_op("dyn.strided_slice")(
+        selected_indices, wildcard(), num_detections, wildcard()
+    )
+    v479 = is_op("dyn.strided_slice")(
+        selected_indices, wildcard(), num_detections, wildcard()
+    )
     v480 = is_op("strided_slice")(v478)
     v481 = is_op("strided_slice")(v479)
     v482 = is_tuple([v480, v481])
@@ -30,7 +44,9 @@ def combined_nms_pattern(boxes, scores, max_boxes_per_class, iou_threshold, scor
     v498 = is_tuple([v480, v497])
     v499 = is_op("concatenate")(v498)
 
-    v500 = is_op("dyn.strided_slice")(selected_indices, wildcard(), num_detections, wildcard())
+    v500 = is_op("dyn.strided_slice")(
+        selected_indices, wildcard(), num_detections, wildcard()
+    )
     v501 = is_op("transpose")(v500)
     v502 = is_op("gather_nd")(wildcard(), v501)
     v503 = is_op("shape_of")(v502)
@@ -55,7 +71,9 @@ def combined_nms_pattern(boxes, scores, max_boxes_per_class, iou_threshold, scor
     v522 = is_op("add")(v517, v520)
     v523 = is_op("where")(v521, v522, v517)
 
-    v524 = is_op("dyn.strided_slice")(selected_indices, wildcard(), num_detections, wildcard())
+    v524 = is_op("dyn.strided_slice")(
+        selected_indices, wildcard(), num_detections, wildcard()
+    )
     v525 = is_op("strided_slice")(v524)
     v526 = is_op("squeeze")(v525)
     v527 = is_op("nn.pad")(v526, wildcard())
@@ -78,28 +96,112 @@ def combined_nms_pattern(boxes, scores, max_boxes_per_class, iou_threshold, scor
     return is_tuple([v539, v540, v541, v542, raw_boxes, raw_scores])
 
 
-def convert_combined_nms(boxes, scores, idxs, iou_thres, num_boxes, indices):
-    scores = op.expand_dims(scores, axis=-1, num_newaxis=1)
-    idxs = op.expand_dims(idxs, axis=-1, num_newaxis=1)
-    idxs = op.cast(idxs, "float32")
-    data = op.concatenate([idxs, scores, boxes], -1)
-    data = op.expand_dims(data, 0, 1)
-    top_k = max_out_size = -1
-    out = op.vision.non_max_suppression(
-        data=data,
-        valid_count=num_boxes,
-        indices=indices,
-        max_output_size=max_out_size,
-        iou_threshold=iou_thres,
-        force_suppress=False,
-        top_k=top_k,
-        coord_start=2,
-        score_index=1,
-        id_index=0,
-        return_indices=True,
-        invalid_to_bottom=False,
+def convert_combined_nms(
+    batch_size,
+    max_output_boxes_per_batch,
+    boxes,
+    scores,
+    max_output_boxes_per_class,
+    iou_threshold,
+    score_threshold,
+    max_total_size,
+    clip_boxes,
+    raw_boxes,
+    raw_scores,
+):
+    (
+        selected_indices,
+        selected_scores,
+        num_detections,
+    ) = _op.vision.all_class_non_max_suppression(
+        boxes,
+        scores,
+        max_output_boxes_per_class,
+        iou_threshold,
+        score_threshold,
+        max_total_size,
+        output_format="tensorflow",
     )
-    return out.tuple_value
+    ret = _expr.TupleWrapper(
+        _expr.Tuple(
+            [
+             selected_indices, selected_scores, num_detections
+            ]
+        ),
+        3,
+    )
+    print("returning")
+    return ret
+
+    box_range = _op.arange(
+        _op.const(0, dtype="int64"),
+        _op.const(max_total_size, dtype="int64"),
+        dtype="int64",
+    )
+    tile_batch_reps = (
+        _op.concatenate([batch_size, 1])
+        if isinstance(batch_size, tvm.tir.Any)
+        else _op.const([batch_size, 1])
+    )
+    box_range_2d = _op.tile(box_range, tile_batch_reps)
+    valid_mask = _op.cast(
+        _op.less(box_range_2d, _op.expand_dims(num_detections, axis=1)), "float32"
+    )
+
+    def select_topk(do_zero_pad):
+        def true_branch():
+            arange = _op.arange(
+                _op.const(0, dtype="int64"),
+                _op.const(max_output_boxes_per_batch, dtype="int64"),
+                dtype="int64",
+            )
+            pad = _op.full(
+                _op.const(0, dtype="int64"),
+                (max_total_size - max_output_boxes_per_batch,),
+            )
+            topk_indices = _op.tile(_op.concatenate([arange, pad], 0), tile_batch_reps)
+            nmsed_scores = _op.gather(selected_scores, 1, topk_indices)
+            nmsed_scores = nmsed_scores * valid_mask
+            return nmsed_scores, topk_indices
+
+        def false_branch():
+            return _op.topk(selected_scores, k=max_total_size, axis=1, ret_type="both")
+
+        # TODO(masahi): support dynamic num_boxes
+        # return _expr.If(do_zero_pad, true_branch(), false_branch())
+        return true_branch() if do_zero_pad else false_branch()
+
+    assert isinstance(
+        max_output_boxes_per_batch, int
+    ), "dynamic number of boxes not supported yet."
+    nmsed_scores, topk_indices = select_topk(
+        max_output_boxes_per_batch < max_total_size
+    )
+
+    indices = _op.take(selected_indices, topk_indices, axis=1, batch_dims=1)
+    nmsed_box_indices = _op.take(indices, _op.const(1), axis=2)
+    nmsed_classes = _op.take(indices, _op.const(0), axis=2)
+    nmsed_boxes = _op.take(boxes, nmsed_box_indices, axis=1, batch_dims=1)
+
+    if clip_boxes:
+        nmsed_boxes = _op.maximum(nmsed_boxes, _expr.const(0, dtype="float32"))
+        nmsed_boxes = _op.minimum(nmsed_boxes, _expr.const(1, dtype="float32"))
+
+    nmsed_boxes = nmsed_boxes * _op.expand_dims(valid_mask, axis=2)
+
+    return _expr.TupleWrapper(
+        _expr.Tuple(
+            [
+                nmsed_boxes,
+                nmsed_scores,
+                nmsed_classes,
+                num_detections,
+                raw_boxes,
+                raw_scores,
+            ]
+        ),
+        6,
+    )
 
 
 class AllClassNMSRewrite(DFPatternCallback):
@@ -121,19 +223,33 @@ class AllClassNMSRewrite(DFPatternCallback):
             self.iou_threshold,
             self.score_threshold,
             self.raw_boxes,
-            self.raw_scores
+            self.raw_scores,
         )
 
     def callback(self, pre, post, node_map):
-        print("matched", node_map[self.max_boxes_per_class])
-        return pre
-        # boxes = node_map[self.boxes][0]
-        # scores = node_map[self.scores][0]
-        # idxs = node_map[self.idxs][0]
-        # iou_thres = node_map[self.iou_threshold][0]
-        # num_boxes = node_map[self.num_boxes][0]
-        # indices = node_map[self.indices][0]
-        # return convert_combined_nms(boxes, scores, idxs, iou_thres, num_boxes, indices)
+        print("matched")
+        boxes = node_map[self.boxes][0]
+        scores = node_map[self.scores][0]
+        max_boxes_per_class = node_map[self.max_boxes_per_class][0]
+        iou_thres = node_map[self.iou_threshold][0]
+        score_thres = node_map[self.score_threshold][0]
+        raw_boxes = node_map[self.raw_boxes][0]
+        raw_scores = node_map[self.raw_scores][0]
+        max_output_boxes_per_batch = 90 * 12804  # TODO
+        print("rewriting")
+        return convert_combined_nms(
+            1,
+            max_output_boxes_per_batch,
+            boxes,
+            scores,
+            max_boxes_per_class,
+            iou_thres,
+            score_thres,
+            100,
+            True,
+            raw_boxes,
+            raw_scores,
+        )
 
 
 def rewrite_all_class_nms(mod):
