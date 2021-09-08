c93d2ed82104494b0b9ebdce2e1d3be68aa5c218
masa <masa@pop-os.localdomain>
2021-09-08 17:53:41 +0900

    int8 import working

--- movenet/movenet.py
+++ movenet/movenet.py
@@ -219,6 +219,30 @@ def load_tvm_model():
     return runtime
 
 
+def load_tvm_int8_model():
+    model_path = "/home/masa/tflite-models/movenet_tflite.onnx"
+    iname = "serving_default_input:0"
+    shape_dict = {iname: [1,256,256,3]}
+    dtype = "uint8"
+    target = "llvm -mcpu=cascadelake"
+
+    dev = tvm.device(target, 0)
+
+    onnx_model = onnx.load(model_path)
+
+    mod, params = relay.frontend.from_onnx(onnx_model, shape_dict, freeze_params=True)
+    mod = relay.transform.DynamicToStatic()(mod)
+    mod = relay.transform.FakeQuantizationToInteger()(mod)
+
+    with tvm.transform.PassContext(opt_level=3):
+        json, lib, params = relay.build(mod, target=target, params=params)
+
+    ctx = tvm.device(target, 0)
+    runtime = tvm.contrib.graph_executor.create(json, lib, ctx)
+    runtime.set_input(**params)
+    return runtime
+
+
 def run_ort(ort_sess, input_image):
     input_image = tf.cast(input_image, dtype=tf.int32)
     onnx_input_dict = {"input": input_image.numpy()}
@@ -233,10 +257,17 @@ def run_tvm(runtime, input_image):
     return runtime.get_output(0).numpy()
 
 
+def run_tvm_int8(runtime, input_image):
+    input_image = tf.cast(input_image, dtype=tf.uint8)
+    runtime.set_input("serving_default_input:0", input_image.numpy())
+    runtime.run()
+    return runtime.get_output(0).numpy()
+
+
 model_path = "../models/movenet_thunder.onnx"
 ort_sess = onnxruntime.InferenceSession(model_path)
 
-runtime = load_tvm_model()
+runtime = load_tvm_int8_model()
 
 image_path = "input_image.jpeg"
 image = tf.io.read_file(image_path)
@@ -247,211 +278,211 @@ input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
 
 keypoint_with_scores = movenet(input_image)
 keypoint_with_scores_ort = run_ort(ort_sess, input_image)
-keypoint_with_scores_tvm = run_tvm(runtime, input_image)
+keypoint_with_scores_tvm = run_tvm_int8(runtime, input_image)
 
 display_image = tf.expand_dims(image, axis=0)
 display_image = tf.cast(
     tf.image.resize_with_pad(display_image, 1280, 1280), dtype=tf.int32
 )
 output_overlay = draw_prediction_on_image(
-    np.squeeze(display_image.numpy(), axis=0), keypoint_with_scores
+    np.squeeze(display_image.numpy(), axis=0), keypoint_with_scores_tvm
 )
 
 plt.figure(figsize=(5, 5))
 plt.imshow(output_overlay)
 _ = plt.axis("off")
-# plt.show()
+plt.show()
 
 
-MIN_CROP_KEYPOINT_SCORE = 0.2
+# MIN_CROP_KEYPOINT_SCORE = 0.2
 
-def init_crop_region(image_height, image_width):
-    if image_width > image_height:
-        box_height = image_width / image_height
-        box_width = 1.0
-        y_min = (image_height / 2 - image_width / 2) / image_height
-        x_min = 0.0
-    else:
-        box_height = 1.0
-        box_width = image_height / image_width
-        y_min = 0.0
-        x_min = (image_width / 2 - image_height / 2) / image_width
+# def init_crop_region(image_height, image_width):
+#     if image_width > image_height:
+#         box_height = image_width / image_height
+#         box_width = 1.0
+#         y_min = (image_height / 2 - image_width / 2) / image_height
+#         x_min = 0.0
+#     else:
+#         box_height = 1.0
+#         box_width = image_height / image_width
+#         y_min = 0.0
+#         x_min = (image_width / 2 - image_height / 2) / image_width
 
-    return {
-        "y_min": y_min,
-        "x_min": x_min,
-        "y_max": y_min + box_height,
-        "x_max": x_min + box_width,
-        "height": box_height,
-        "width": box_width,
-    }
-
-
-def torso_visible(keypoints):
-    """Checks whether there are enough torso keypoints.
-
-    This function checks whether the model is confident at predicting one of the
-    shoulders/hips which is required to determine a good crop region.
-    """
-    return (
-        keypoints[0, 0, KEYPOINT_DICT["left_hip"], 2] > MIN_CROP_KEYPOINT_SCORE
-        or keypoints[0, 0, KEYPOINT_DICT["right_hip"], 2] > MIN_CROP_KEYPOINT_SCORE
-    ) and (
-        keypoints[0, 0, KEYPOINT_DICT["left_shoulder"], 2] > MIN_CROP_KEYPOINT_SCORE
-        or keypoints[0, 0, KEYPOINT_DICT["right_shoulder"], 2] > MIN_CROP_KEYPOINT_SCORE
-    )
+#     return {
+#         "y_min": y_min,
+#         "x_min": x_min,
+#         "y_max": y_min + box_height,
+#         "x_max": x_min + box_width,
+#         "height": box_height,
+#         "width": box_width,
+#     }
 
 
-def determine_torso_and_body_range(keypoints, target_keypoints, center_y, center_x):
-    torso_joints = ["left_shoulder", "right_shoulder", "left_hip", "right_hip"]
-    max_torso_yrange = 0.0
-    max_torso_xrange = 0.0
-    for joint in torso_joints:
-        dist_y = abs(center_y - target_keypoints[joint][0])
-        dist_x = abs(center_x - target_keypoints[joint][1])
-        if dist_y > max_torso_yrange:
-            max_torso_yrange = dist_y
-        if dist_x > max_torso_xrange:
-            max_torso_xrange = dist_x
+# def torso_visible(keypoints):
+#     """Checks whether there are enough torso keypoints.
 
-    max_body_yrange = 0.0
-    max_body_xrange = 0.0
-    for joint in KEYPOINT_DICT.keys():
-        if keypoints[0, 0, KEYPOINT_DICT[joint], 2] < MIN_CROP_KEYPOINT_SCORE:
-            continue
-        dist_y = abs(center_y - target_keypoints[joint][0])
-        dist_x = abs(center_x - target_keypoints[joint][1])
-        if dist_y > max_body_yrange:
-            max_body_yrange = dist_y
-
-        if dist_x > max_body_xrange:
-            max_body_xrange = dist_x
-
-    return [max_torso_yrange, max_torso_xrange, max_body_yrange, max_body_xrange]
+#     This function checks whether the model is confident at predicting one of the
+#     shoulders/hips which is required to determine a good crop region.
+#     """
+#     return (
+#         keypoints[0, 0, KEYPOINT_DICT["left_hip"], 2] > MIN_CROP_KEYPOINT_SCORE
+#         or keypoints[0, 0, KEYPOINT_DICT["right_hip"], 2] > MIN_CROP_KEYPOINT_SCORE
+#     ) and (
+#         keypoints[0, 0, KEYPOINT_DICT["left_shoulder"], 2] > MIN_CROP_KEYPOINT_SCORE
+#         or keypoints[0, 0, KEYPOINT_DICT["right_shoulder"], 2] > MIN_CROP_KEYPOINT_SCORE
+#     )
 
 
-def determine_crop_region(keypoints, image_height, image_width):
-    target_keypoints = {}
-    for joint in KEYPOINT_DICT.keys():
-        target_keypoints[joint] = [
-            keypoints[0, 0, KEYPOINT_DICT[joint], 0] * image_height,
-            keypoints[0, 0, KEYPOINT_DICT[joint], 1] * image_width,
-        ]
+# def determine_torso_and_body_range(keypoints, target_keypoints, center_y, center_x):
+#     torso_joints = ["left_shoulder", "right_shoulder", "left_hip", "right_hip"]
+#     max_torso_yrange = 0.0
+#     max_torso_xrange = 0.0
+#     for joint in torso_joints:
+#         dist_y = abs(center_y - target_keypoints[joint][0])
+#         dist_x = abs(center_x - target_keypoints[joint][1])
+#         if dist_y > max_torso_yrange:
+#             max_torso_yrange = dist_y
+#         if dist_x > max_torso_xrange:
+#             max_torso_xrange = dist_x
 
-    if torso_visible(keypoints):
-        center_y = (
-            target_keypoints["left_hip"][0] + target_keypoints["right_hip"][0]
-        ) / 2
-        center_x = (
-            target_keypoints["left_hip"][1] + target_keypoints["right_hip"][1]
-        ) / 2
+#     max_body_yrange = 0.0
+#     max_body_xrange = 0.0
+#     for joint in KEYPOINT_DICT.keys():
+#         if keypoints[0, 0, KEYPOINT_DICT[joint], 2] < MIN_CROP_KEYPOINT_SCORE:
+#             continue
+#         dist_y = abs(center_y - target_keypoints[joint][0])
+#         dist_x = abs(center_x - target_keypoints[joint][1])
+#         if dist_y > max_body_yrange:
+#             max_body_yrange = dist_y
 
-        (
-            max_torso_yrange,
-            max_torso_xrange,
-            max_body_yrange,
-            max_body_xrange,
-        ) = determine_torso_and_body_range(
-            keypoints, target_keypoints, center_y, center_x
-        )
+#         if dist_x > max_body_xrange:
+#             max_body_xrange = dist_x
 
-        crop_length_half = np.amax(
-            [
-                max_torso_xrange * 1.9,
-                max_torso_yrange * 1.9,
-                max_body_yrange * 1.2,
-                max_body_xrange * 1.2,
-            ]
-        )
-
-        tmp = np.array(
-            [center_x, image_width - center_x, center_y, image_height - center_y]
-        )
-        crop_length_half = np.amin([crop_length_half, np.amax(tmp)])
-
-        crop_corner = [center_y - crop_length_half, center_x - crop_length_half]
-
-        if crop_length_half > max(image_width, image_height) / 2:
-            return init_crop_region(image_height, image_width)
-        else:
-            crop_length = crop_length_half * 2
-            return {
-                "y_min": crop_corner[0] / image_height,
-                "x_min": crop_corner[1] / image_width,
-                "y_max": (crop_corner[0] + crop_length) / image_height,
-                "x_max": (crop_corner[1] + crop_length) / image_width,
-                "height": (crop_corner[0] + crop_length) / image_height
-                - crop_corner[0] / image_height,
-                "width": (crop_corner[1] + crop_length) / image_width
-                - crop_corner[1] / image_width,
-            }
-    else:
-        return init_crop_region(image_height, image_width)
+#     return [max_torso_yrange, max_torso_xrange, max_body_yrange, max_body_xrange]
 
 
-def crop_and_resize(image, crop_region, crop_size):
-    """Crops and resize the image to prepare for the model input."""
-    boxes = [
-        [
-            crop_region["y_min"],
-            crop_region["x_min"],
-            crop_region["y_max"],
-            crop_region["x_max"],
-        ]
-    ]
-    output_image = tf.image.crop_and_resize(
-        image, box_indices=[0], boxes=boxes, crop_size=crop_size
-    )
-    return output_image
+# def determine_crop_region(keypoints, image_height, image_width):
+#     target_keypoints = {}
+#     for joint in KEYPOINT_DICT.keys():
+#         target_keypoints[joint] = [
+#             keypoints[0, 0, KEYPOINT_DICT[joint], 0] * image_height,
+#             keypoints[0, 0, KEYPOINT_DICT[joint], 1] * image_width,
+#         ]
+
+#     if torso_visible(keypoints):
+#         center_y = (
+#             target_keypoints["left_hip"][0] + target_keypoints["right_hip"][0]
+#         ) / 2
+#         center_x = (
+#             target_keypoints["left_hip"][1] + target_keypoints["right_hip"][1]
+#         ) / 2
+
+#         (
+#             max_torso_yrange,
+#             max_torso_xrange,
+#             max_body_yrange,
+#             max_body_xrange,
+#         ) = determine_torso_and_body_range(
+#             keypoints, target_keypoints, center_y, center_x
+#         )
+
+#         crop_length_half = np.amax(
+#             [
+#                 max_torso_xrange * 1.9,
+#                 max_torso_yrange * 1.9,
+#                 max_body_yrange * 1.2,
+#                 max_body_xrange * 1.2,
+#             ]
+#         )
+
+#         tmp = np.array(
+#             [center_x, image_width - center_x, center_y, image_height - center_y]
+#         )
+#         crop_length_half = np.amin([crop_length_half, np.amax(tmp)])
+
+#         crop_corner = [center_y - crop_length_half, center_x - crop_length_half]
+
+#         if crop_length_half > max(image_width, image_height) / 2:
+#             return init_crop_region(image_height, image_width)
+#         else:
+#             crop_length = crop_length_half * 2
+#             return {
+#                 "y_min": crop_corner[0] / image_height,
+#                 "x_min": crop_corner[1] / image_width,
+#                 "y_max": (crop_corner[0] + crop_length) / image_height,
+#                 "x_max": (crop_corner[1] + crop_length) / image_width,
+#                 "height": (crop_corner[0] + crop_length) / image_height
+#                 - crop_corner[0] / image_height,
+#                 "width": (crop_corner[1] + crop_length) / image_width
+#                 - crop_corner[1] / image_width,
+#             }
+#     else:
+#         return init_crop_region(image_height, image_width)
 
 
-def run_inference(movenet, image, crop_region, crop_size):
-    image_height, image_width, _ = image.shape
-    input_image = crop_and_resize(
-        tf.expand_dims(image, axis=0), crop_region, crop_size=crop_size
-    )
-    # Run model inference.
-    # keypoints_with_scores = movenet(input_image)
-    keypoints_with_scores = run_tvm(runtime, input_image)
-    # Update the coordinates.
-    for idx in range(17):
-        keypoints_with_scores[0, 0, idx, 0] = (
-            crop_region["y_min"] * image_height
-            + crop_region["height"] * image_height * keypoints_with_scores[0, 0, idx, 0]
-        ) / image_height
-        keypoints_with_scores[0, 0, idx, 1] = (
-            crop_region["x_min"] * image_width
-            + crop_region["width"] * image_width * keypoints_with_scores[0, 0, idx, 1]
-        ) / image_width
-    return keypoints_with_scores
+# def crop_and_resize(image, crop_region, crop_size):
+#     """Crops and resize the image to prepare for the model input."""
+#     boxes = [
+#         [
+#             crop_region["y_min"],
+#             crop_region["x_min"],
+#             crop_region["y_max"],
+#             crop_region["x_max"],
+#         ]
+#     ]
+#     output_image = tf.image.crop_and_resize(
+#         image, box_indices=[0], boxes=boxes, crop_size=crop_size
+#     )
+#     return output_image
 
-image_path = "dance.gif"
-image = tf.io.read_file(image_path)
-image = tf.image.decode_gif(image)
 
-num_frames, image_height, image_width, _ = image.shape
-crop_region = init_crop_region(image_height, image_width)
+# def run_inference(movenet, image, crop_region, crop_size):
+#     image_height, image_width, _ = image.shape
+#     input_image = crop_and_resize(
+#         tf.expand_dims(image, axis=0), crop_region, crop_size=crop_size
+#     )
+#     # Run model inference.
+#     # keypoints_with_scores = movenet(input_image)
+#     keypoints_with_scores = run_tvm(runtime, input_image)
+#     # Update the coordinates.
+#     for idx in range(17):
+#         keypoints_with_scores[0, 0, idx, 0] = (
+#             crop_region["y_min"] * image_height
+#             + crop_region["height"] * image_height * keypoints_with_scores[0, 0, idx, 0]
+#         ) / image_height
+#         keypoints_with_scores[0, 0, idx, 1] = (
+#             crop_region["x_min"] * image_width
+#             + crop_region["width"] * image_width * keypoints_with_scores[0, 0, idx, 1]
+#         ) / image_width
+#     return keypoints_with_scores
 
-output_images = []
-for frame_idx in range(num_frames):
-    keypoints_with_scores = run_inference(
-        movenet,
-        image[frame_idx, :, :, :],
-        crop_region,
-        crop_size=[input_size, input_size],
-    )
-    output_images.append(
-        draw_prediction_on_image(
-            image[frame_idx, :, :, :].numpy().astype(np.int32),
-            keypoints_with_scores,
-            crop_region=None,
-            close_figure=True,
-            output_image_height=300,
-        )
-    )
-    crop_region = determine_crop_region(
-        keypoints_with_scores, image_height, image_width
-    )
+# image_path = "dance.gif"
+# image = tf.io.read_file(image_path)
+# image = tf.image.decode_gif(image)
 
-output = np.stack(output_images, axis=0)
-to_gif(output, fps=10)
+# num_frames, image_height, image_width, _ = image.shape
+# crop_region = init_crop_region(image_height, image_width)
+
+# output_images = []
+# for frame_idx in range(num_frames):
+#     keypoints_with_scores = run_inference(
+#         movenet,
+#         image[frame_idx, :, :, :],
+#         crop_region,
+#         crop_size=[input_size, input_size],
+#     )
+#     output_images.append(
+#         draw_prediction_on_image(
+#             image[frame_idx, :, :, :].numpy().astype(np.int32),
+#             keypoints_with_scores,
+#             crop_region=None,
+#             close_figure=True,
+#             output_image_height=300,
+#         )
+#     )
+#     crop_region = determine_crop_region(
+#         keypoints_with_scores, image_height, image_width
+#     )
+
+# output = np.stack(output_images, axis=0)
+# to_gif(output, fps=10)
